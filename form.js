b050e0a7173e0edff4bcf8a8de9ee6df9513e966
Wakabataku <wakaba_taku666@yahoo.co.jp>
2020-12-10 14:04:27 +0900

    入力できるようにした

--- form.js
+++ form.js
@@ -1,12 +1,32 @@
 $(function(){
+
+    //タブを指定
+    $("#tabs").tabs();
+
+    //クリックされたときにテキストエリアに入力
+    $('#often').children().on('click', function(){
+        let word = $(this).text();
+        console.log(word);
+        $('textarea').append(word+'\n');
+    });
     //語句をドラッグ可能にする
-    $(".hand").draggable({helper: "clone"});
+    $('#often').children().draggable({helper: "clone"});
 
     //語句がドロップされたときの処理
-    $(".main").droppable({
-        //dropイベント
+    $("#main").find('textarea').droppable({
         drop: function(event, ui){
-            
+            //ドラッグされた要素情報取得
+            let selectedWord = ui.draggable.text();;
+            $('textarea').append(selectedWord+ '\n');
+            // console.log(selectedWord);
         }
     })
-})
+
+    // //語句がドロップされたときの処理
+    // $(".main").droppable({
+    //     //dropイベント
+    //     drop: function(event, ui){
+    //         let selectedSrc = ui.draggable.attr("href");
+    //     }
+    // });
+});
